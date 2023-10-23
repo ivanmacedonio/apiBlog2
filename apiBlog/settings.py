ddb18578567251a7b2a265903ad59e1b7ce7e7e3
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-10-23 09:37:54 -0300

    cors fix

--- apiBlog/settings.py
+++ apiBlog/settings.py
@@ -147,4 +147,5 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
+CORS_ALLOW_ALL_ORIGINS = True
+
